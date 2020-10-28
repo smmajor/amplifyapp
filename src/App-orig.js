2d29e1e2f78a77edf95ebe0a1a51f2c9b626a423
MAJOR, SHAWN <SHAWN.MAJOR@LibertyMutual.com>
2020-10-28 11:05:08 -0700

    add notes graphql

---
+++ src/App-orig.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Is it still working?</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
